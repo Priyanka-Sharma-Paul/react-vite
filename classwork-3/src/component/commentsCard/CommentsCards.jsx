68c4411628a81deaac46f7c09406ed053aaefeb8
Priyanka Sharma Paul <priyankasharmapaharpur@gmail.com>
2024-12-21 22:52:43 +0530

    Fixes

--- classwork-3/src/component/commentsCard/CommentsCards.jsx
+++ classwork-3/src/component/commentsCard/CommentsCards.jsx
@@ -1,26 +1,25 @@
 import Comments from "../../data/comments";
-
+import Styles from "./CommentsCard.module.css"
 
 const CommentsCards = () => {
     return (
-        <div>
+        <div className={Styles.CommentsContainer}>
             {
                 Comments.map((comment, index) => {
                     return (
-                        <div key={index} className="comment-card">
-                            <div>{comment.comment}</div>
-                            <div>
-                            <img src={comment.imageUrl} alt={`${comment.name}'s profile`} />
-                            <div>
-                                <span>{comment.name}</span>
-                                <span>{comment.position}</span>
-                                
-                            </div>
+                        <div key={index} className={Styles.commentCard}>
+                            <div className={Styles.comment}>{comment.comment}</div>
+                            <div className={Styles.commentProfileContainer}>
+                                 <img className={Styles.commentProfileImg} src={comment.imageUrl} alt={`${comment.name}'s profile`} />
+                                <div className={Styles.commentProfileText}>
+                                    <span className={Styles.commentProfileName}>{comment.name}</span>
+                                    <span className={Styles.commentProfilePosition}>{comment.position}</span>
+                                </div>
 
                             </div>
-                            
 
-                           
+
+
 
 
                         </div>
