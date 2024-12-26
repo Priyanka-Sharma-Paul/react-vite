9843cdda24175ea39cee155a6983c3ec93c47f18
Priyanka Sharma Paul <priyankasharmapaharpur@gmail.com>
2024-12-26 13:21:54 +0530

    Changes

--- homework-5/src/components/Imagegallery/ImageGallery.jsx
+++ homework-5/src/components/Imagegallery/ImageGallery.jsx
@@ -1,22 +1,36 @@
 import styles from "./ImageGallery.module.css";
 import images from "../../data/images";
+import { useState } from 'react';
 
 const ImageGallery = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleClick = (image) => {
+        setSelectedImage(image);
+    };
+
     return (
         <div>
-            
+            <p className={styles.galleryPara}>Click on an image!</p>
             <div className={styles.imageContainer}>
-              {
-                images.map((url, index) => {
-                    return (
-                        <div key={index} className={styles.imageCard}>
-                            <img src={url}/>
-                        </div>
-                    )
-                })
-              }  
+                {
+                    images.map((url, index) => {
+                        return (
+                            <div key={index} className={styles.imageCard} onClick={() => handleClick(url)}>
+                                <img src={url} />
+                            </div>
+                        )
+                    })
+                }
 
             </div>
+
+            {selectedImage && (
+                <div className={styles.selectedImage}>
+                    <h2 className={styles.selectedHeading}>Selected Image</h2>
+                    <img src={selectedImage} alt="Selected" className={styles.selectedImg} />
+                </div>
+            )}
 
         </div>
 
