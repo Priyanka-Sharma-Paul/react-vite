c6101acbda6e81f189bf8fb962506d62d64c83d0
Priyanka Sharma Paul <priyankasharmapaharpur@gmail.com>
2024-12-26 13:35:38 +0530

    css

--- homework-5/src/App.jsx
+++ homework-5/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ImageGallery from './components/Imagegallery/ImageGallery'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   
 
   return (
     <>
-      < ImageGallery/>
+      <Navbar/>
+      <ImageGallery/>
     </>
   )
 }
---
+++ homework-5/src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from './Navbar.module.css'
+
+const Navbar = () => {
+    return (
+      <nav className={styles.navbar}>
+        <h1>Image Gallery</h1>
+      </nav>
+    );
+  };
+
+  export default Navbar;
